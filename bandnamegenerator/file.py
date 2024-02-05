443432069f302f0a93250994c48c3baa4dd585ed
Fadhli Abrar <fadhliabrar.dev@gmail.com>
2024-02-05 18:22:01 +0700

    py day 1 gabut skripsi

---
+++ bandnamegenerator/file.py
@@ -0,0 +1,6 @@
+def func():
+    inputCity = input("Di mana kota tempat anda dibesarkan?\n")
+    inputPetsName = input("Apa nama hewan peliharaanmu?\n")
+    print("Nama bandmu adalah", inputCity, inputPetsName)
+
+func()
